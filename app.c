db16fb7a7b3affb12aaac8990c04297f91dcb55b
Tejas Chopra <chopratejas@gmail.com>
2013-04-10 16:56:34 -0700

    Update app.c code

--- app.c
+++ app.c
@@ -4,9 +4,9 @@
 
 int main()
 {
-  printf("Hello world!\n");
+  printf("Simple File Caching!\n");
   char *file[4];
-  file[0] = "file";
+  file[0] = "file1";
   file[1] = "file2";
   file[2] = "file3";
   file[3] = "file4";
@@ -16,7 +16,6 @@ int main()
   file_cache_pin_files(cache, (const char **)file, 3);
   file_cache_unpin_files(cache, (const char **) file, 2);
   file_cache_evict(cache); 
-  //file_cache_pin_files(cache, (const char **)file, 4);
 
   printf("Destroying cache!\n");
   file_cache_destroy(cache);
